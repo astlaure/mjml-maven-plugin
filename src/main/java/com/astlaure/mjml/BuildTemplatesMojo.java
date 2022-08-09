b9fcad8767f75e95411b752f96781942443c0bd4
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2022-08-09 09:21:27 -0400

    documentation 📃

--- src/main/java/com/astlaure/mjml/BuildTemplatesMojo.java
+++ src/main/java/com/astlaure/mjml/BuildTemplatesMojo.java
@@ -15,24 +15,42 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Mojo to compile mjml templates to html using the nodejs official package.
+ */
 @Mojo(name = "build-templates", defaultPhase = LifecyclePhase.COMPILE)
 public class BuildTemplatesMojo extends AbstractMojo {
     private final FileService fileService = new FileService();
     private final NpmService npmService = new NpmService();
     private final CommandService commandService = new CommandService();
 
+    /**
+     * Location of the source (.mjml) files
+     */
     @Parameter(property = "mjml.source")
     private String source;
 
+    /**
+     * Location of the compiled (.ftl) files
+     */
     @Parameter(property = "mjml.destination")
     private String destination;
 
+    /**
+     * Extension of the outputted files. Override the default .ftl extension
+     */
     @Parameter(property = "mjml.extension")
     private String extension;
 
+    /**
+     * Rewrite the script files on every run.
+     */
     @Parameter(property = "mjml.override", defaultValue = "false")
     private boolean override;
 
+    /**
+     * Cleanup the copied and node_modules after execution.
+     */
     @Parameter(property = "mjml.temporary", defaultValue = "false")
     private boolean temporary;
 
